107b224e5d279c92193480ca648b156a4b74dc3e
donny.hurley@atu.ie <donny.hurley@atu.ie@S-9WR36V3>
2025-11-27 16:51:33 +0000

    some updates

--- SpaceShoot/MainPage.xaml.cs
+++ SpaceShoot/MainPage.xaml.cs
@@ -45,7 +45,10 @@ public partial class MainPage : ContentPage
         gameTimer = Dispatcher.CreateTimer();
         gameTimer.Interval = TimeSpan.FromMilliseconds(16);
         gameTimer.Tick += OnGameTick;
-        gameTimer.IsRepeating = true;
+
+        enemySpawnTimer = Dispatcher.CreateTimer();
+        enemySpawnTimer.Interval = TimeSpan.FromSeconds(2);
+        enemySpawnTimer.Tick += OnEnemySpawn;
 
     }
 
@@ -73,7 +76,7 @@ public partial class MainPage : ContentPage
         GameOverOverlay.IsVisible = false;
         StartButton.IsEnabled = false;
         gameTimer.Start();
-        //enemySpawnTimer.Start();
+        enemySpawnTimer.Start();
 
         UpdateUI();
 
@@ -223,7 +226,7 @@ public partial class MainPage : ContentPage
     }
 
     private void UpdateUI() {
-
+        LivesLabel.Text = $"Lives : {lives}";
     }
 
     private void EndGame() {
